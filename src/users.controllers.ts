b8caaa91b6f35cde416a6f8da99011d9aa115106
Raman Kumar <ramankr7321@gmail.com>
2026-02-16 11:18:10 +0530

    Added complete NestJS project

---
+++ src/users.controllers.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from "@nestjs/common";
+import { get } from "http";
+@Controller("/users")
+
+export class UsersController {
+    @Get("/profile")
+    getProfile(){
+        return "Hello Raman";
+
+    }
+}
